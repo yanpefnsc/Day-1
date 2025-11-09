b0aa701492fbb55488536d2ea44447d4e46c15d9
yanpefnsc <leonardofonsecadmc@gmail.com>
2025-11-08 22:06:03 -0300

    Add files via upload

---
+++ Day01.py/test.py
@@ -0,0 +1,6 @@
+name = input("Enter your full name: ")
+start_time = input("Enter the time you started your workday (e.g., 08:00): ")
+tasks_completed = int(input("How many tasks have you completed so far? "))
+
+print("Hello, " + name + "! You started your workday at " + start_time + ".")
+print("So far you have completed", tasks_completed, "tasks.")
